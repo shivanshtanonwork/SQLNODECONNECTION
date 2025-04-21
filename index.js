1a7c8e2de70b629a542733c0152fd8fe6dc976c6
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-04-21 13:03:59 +0530

    Insert in bulk

--- index.js
+++ index.js
@@ -9,14 +9,29 @@ const connection = mysql.createConnection({
     password: 'Shiv@nsh@150399',
 });
 
+let getRandomUser = () => {
+    return [
+        faker.string.uuid(),
+        faker.internet.username(),
+        faker.internet.email(),
+        faker.internet.password(),
+    ];
+};
+
+
 let q = "INSERT INTO user(id,username, email, password) VALUES ?" //query
 // let users = [
 //     ["123b", "123_newuserb", "abc@gmail.comb", "abcb"],
 //     ["123c", "123_newuserc", "abc@gmail.comc", "abcc"],
 // ];
+let data = [];
+for (let i = 0; i <= 100; i++) {
+    data.push(getRandomUser()); //100 fake users data
+
+}
 
 try {
-    connection.query(q, [users], (err, result) => {
+    connection.query(q, [data], (err, result) => {
         if (err) throw err;
         console.log(result);
     })
@@ -28,13 +43,3 @@ try {
 
 connection.end();
 
-let getRandomUser = () => {
-    return [
-        faker.string.uuid(),
-        faker.internet.username(),
-        faker.internet.email(),
-        faker.internet.password(),
-    ];
-};
-
-// console.log(getRandomUser());
